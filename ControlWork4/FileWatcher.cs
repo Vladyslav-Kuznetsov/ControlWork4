ca4c30710da92c2276f36594a402105a69033870
Vladislav Kuznetsov <vlad.kuznetsov.97@gmail.com>
2019-10-19 12:47:30 +0300

    Add project files.

---
+++ ControlWork4/FileWatcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ControlWork4
+{
+    public class FileWatcher
+    {
+        public event Action Change;
+        private object _loker;
+        private string _path;
+        private DateTime _lastWriteTime;
+
+        public FileWatcher(string path, object loker)
+        {
+            _path = path;
+            _loker = loker;
+            _lastWriteTime = File.GetLastWriteTime(_path);
+        }
+
+        public void Start()
+        {
+            DateTime lastWriteTime;
+
+            while (true)
+            {
+                lock (_loker)
+                {
+                    lastWriteTime = File.GetLastWriteTime(_path);
+                }
+
+                if (_lastWriteTime != lastWriteTime)
+                {
+                    _lastWriteTime = File.GetLastWriteTime(_path);
+                    Task.Run(() => Change?.Invoke());
+                }
+            }
+        }
+    }
+}
---
+++ ControlWork4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ControlWork4
+{
+    public class Program
+    {
+        private static object _loker = new object();
+
+        public static void Main(string[] args)
+        {
+            File.Create("1.txt").Dispose();
+            FileWatcher fileWatcher = new FileWatcher("1.txt", _loker);
+            fileWatcher.Change += Change;
+            Task.Run(() => fileWatcher.Start());
+
+            while (true)
+            {
+                Console.WriteLine("Do you want 1?");
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() == "yes")
+                {
+                    lock (_loker)
+                    {
+                        File.WriteAllText("1.txt", "1");
+                    }
+                }
+            }
+        }
+
+        private static void Change()
+        {
+            Console.WriteLine("Call event");
+            string str = string.Empty;
+
+            lock (_loker)
+            {
+                str = File.ReadAllText("1.txt");
+            }
+
+            if (str == "1")
+            {
+                lock (_loker)
+                {
+                    File.WriteAllText("1.txt", "0");
+                }
+
+                Thread.Sleep(10000);
+                Console.WriteLine("File content changed");
+            }
+        }
+    }
+}
